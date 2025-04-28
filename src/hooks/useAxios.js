e23eeb9d04c50ea64a77579034979d061c407a90
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-04-28 14:30:16 +0200

    created useAxios hook

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useAxios(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    //deklarerar en abortController jag kan använda sen. Används för att avbryta pågående API anrop när man
+    // t.ex. försöker uppdatera ett state i en komponent som demountats.
+    const controller = new AbortController();
+
+    //lyssnar efter eventuell signal från min controller.
+    axios
+      .get(url, { signal: controller.signal })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        if (axios.isCancel(error)) {
+          console.log(`Anrop avbrutet`);
+        } else {
+          setError(error.message);
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useAxios;
