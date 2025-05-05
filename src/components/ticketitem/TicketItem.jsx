6d038d03610325d445bbdef9b2c2f6c4618d99e1
Annelie Östelid <Annelie.ostelid@outlook.com>
2025-05-05 22:38:06 +0200

    styling card

--- src/components/ticketitem/TicketItem.jsx
+++ src/components/ticketitem/TicketItem.jsx
@@ -4,11 +4,24 @@ import "./ticketItem.css";
 function TicketItem({ ticket }) {
   return (
     <li className="ticket__list-item">
+      <p className="ticket__category ticket__category--padding">WHAT</p>
       <h2 className="ticket__title">{ticket.name}</h2>
-      <p className="ticket__paragraph">
-        {ticket.when.date} kl {ticket.when.from} - {ticket.when.to}
-      </p>
-      <p>@ {ticket.where}</p>
+      <hr className="ticket__stroke" />
+      <p className="ticket__category ticket__category--padding">WHERE</p>
+      <p className="ticket__paragraph-where">{ticket.where}</p>
+      <hr className="ticket__stroke" />
+      <section className="ticket__time">
+        <p className="ticket__category">WHEN</p>
+        <p className="ticket__paragraph">{ticket.when.date}</p>
+      </section>
+      <section className="ticket__time">
+        <p className="ticket__category">FROM</p>
+        <p className="ticket__paragraph">{ticket.when.from}</p>
+      </section>{" "}
+      <section className="ticket__time">
+        <p className="ticket__category">TO</p>
+        <p className="ticket__paragraph">{ticket.when.to}</p>
+      </section>
       <p>Pris: {ticket.price} SEK</p>
       <p>Antal: {ticket.count}</p>
     </li>
